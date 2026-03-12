d9cf4005ec5b2c8803459713bd6761ed685e37f4
Sakthivel <148680319+sakthivelk97@users.noreply.github.com>
2026-03-12 14:39:02 +0530

    Comment out Snowflake session import and add connection

--- steamlit_apps.py
+++ steamlit_apps.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 
@@ -11,7 +11,8 @@ st.write(
   """
 )
 
-import streamlit as st
+cnx = st.connection("snoflake")
+session = cnx.session()
 
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name on your Smoothie will be:", name_on_order)
